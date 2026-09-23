6adb336f1305135609bead82d11bc3ec4bb7762d
Sara-e23 <22151232@aguascalientes.tecnm.mx>
2026-09-23 12:46:08 -0600

    Segundo commit, Miercoles 23/09

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -55,7 +55,7 @@ class InscripcionViewSet(viewsets.ModelViewSet):
 class CustomAuthToken(ObtainAuthToken):
     def post(self, request, *args, **bargs):
         serializer = self.serializer_class(data=request.data,
-                                           content={'request': request})
+                                           context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
